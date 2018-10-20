70baab81220c830bbaefe780a05fd4f4026b17c0
JohnFiddleystein <patrickantonisawesome@gmail.com>
2018-10-21 00:45:50 +0100

    intial push

---
+++ Assets/Scripts/OnionWakeUp.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnionWakeUp : MonoBehaviour {
+
+	public Animator animHandler;
+    public GameObject player;
+
+	IEnumerator waitUntilEnd() {
+
+		animHandler.SetBool ("InTrigger", true);
+		yield return new WaitForSeconds (5.1f);
+		animHandler.SetBool ("CutScenePlayed", true);
+	}
+
+	void OnTriggerEnter(Collider col) {
+		if (col.tag == "Player") {
+			StartCoroutine (waitUntilEnd ());
+		}
+	}
+
+}
