b1928cf3a6e36d69f287709185529eb81e0d8302
JohnFiddleystein <patrickantonisawesome@gmail.com>
2018-11-17 01:54:18 +0000

    made some more changes towards the impact site

--- Assets/Scripts/FileSelectController.cs
+++ Assets/Scripts/FileSelectController.cs
@@ -1,10 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FileSelectController : MonoBehaviour {
-    void DataFileClick()
-    {
 
+    public Button[] buttons;
+    private Animator anim;
+    private bool loadingLevel;
+
+    void Start()
+    {
+        buttons[0].onClick.AddListener(delegate { whenClicked(buttons[0].gameObject, 0); });
+        buttons[1].onClick.AddListener(delegate { whenClicked(buttons[1].gameObject, 1); });
+        buttons[2].onClick.AddListener(delegate { whenClicked(buttons[2].gameObject, 2); });
+    }
+
+    void whenClicked(GameObject button, int index)
+    {
+        anim = button.GetComponent<Animator>();
+        anim.SetBool("Clicked", true);
+        
     }
 }
