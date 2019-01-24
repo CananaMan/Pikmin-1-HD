c4e0b25ca5d40da8c838ff9e4965f02ab9b2595a
JohnFiddleystein <patrickantonisawesome@gmail.com>
2019-01-24 00:00:43 +0000

    qe

---
+++ Assets/Scripts/Pikmin/formationMgr.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class formationMgr : MonoBehaviour
+{
+    public List<GameObject> pikiInForm; // all pikmin that have a formation position
+    private bool isInPos; // bool to detect if pikmin[i] are in formation
+    public bool goToOriginFormation; // If this gets called then they have to go to the original formation
+    public float[] posTable;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
